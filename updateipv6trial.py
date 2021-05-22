a8308a625207cbf0286263d245ae4402f1a6eecd
test2a <39449563+test2a@users.noreply.github.com>
2021-05-22 15:03:31 +0530

    initial commit. it is not working fully ATM
    final text box is giving me some trouble. investigating

---
+++ updateipv6trial.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome("chromedriver")
+driver.get("http://192.168.29.1");
+
+
+
+username_input ='//*[@id="tf1_userName"]'
+password_input='//*[@id="tf1_password"]'
+login_submit='/html/body/div[1]/div/div/div[2]/form/div/div[5]/button'
+security='//*[@id="mainMenu4"]'
+firewall='//*[@id="tf1_security_defaultPolicy"]'
+ipv6firewall='//*[@id="main"]/div[6]/ul/li[3]/a'
+rdlistitem='//*[@id="3"]'
+edit3editem='//*[@id="editMenu"]'
+
+ipv6inputmodal='//*[@id="tf1_dialog"]/div[2]'
+driver.find_element_by_xpath(username_input).send_keys('admin')
+driver.find_element_by_xpath(password_input).send_keys('Testing@123')
+driver.find_element_by_xpath(login_submit).click()
+time.sleep(8)
+driver.find_element_by_xpath(security).click()
+time.sleep(3)
+driver.find_element_by_xpath(firewall).click()
+time.sleep(6)
+driver.find_element_by_xpath(ipv6firewall).click()
+time.sleep(6)
+driver.find_element_by_xpath(rdlistitem).click()
+id3edit=driver.find_element_by_xpath('//*[@id="3"]')
+ActionChains(driver).move_to_element(id3edit).context_click(id3edit).perform()
+
+driver.find_element_by_xpath(edit3editem).click()
+time.sleep(4)
+driver.find_element_by_xpath(ipv6inputmodal).click()
+
+id3inputbox=driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[6]/div/form/div/div[2]/div[18]/input')
+ActionChains(driver).move_to_element(id3inputbox).send_keys(keys.TAB*3).perform()
